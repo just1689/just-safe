d3424e9bf4f92ba29e04661b18c81f17225f0c86
Just1689 <Just1689@gmail.com>
2020-07-12 16:38:50 +0200

    handle site entry upserts

--- model/site.go
+++ model/site.go
@@ -1,8 +1,36 @@
 package model
 
+import "github.com/sirupsen/logrus"
+
 type Site struct {
 	Site    string  `json:"site"`
 	Entries []Entry `json:"entries"`
+}
+
+func (s *Site) AddItem(username, password string) {
+	found := false
+	var i int
+
+	for row, next := range s.Entries {
+		if next.Username == username {
+			i = row
+			found = true
+			break
+		}
+	}
+
+	if found {
+		logrus.Println("set old")
+		s.Entries[i].Password = password
+		return
+	}
+
+	logrus.Println("set new")
+	s.Entries = append(s.Entries, Entry{
+		Username: username,
+		Password: password,
+	})
+
 }
 
 type Entry struct {
