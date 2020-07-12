ad17647177fc010f1d257db874849c594a0f909a
Just1689 <Just1689@gmail.com>
2020-07-12 19:44:41 +0200

    GetSessionFromBytes and GetSessionFilename

--- model/session.go
+++ model/session.go
@@ -1,6 +1,29 @@
 package model
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"time"
+)
+
 type Session struct {
 	PrivateKey string `json:"privateKey"`
 	PublicKey  string `json:"publicKey"`
+}
+
+func GetSessionFilename() string {
+	n := time.Now()
+	return fmt.Sprintf("session.%v-%v-%v.json", n.Year(), n.Month(), n.Day())
+}
+
+func GetSessionFromBytes(b []byte) *Session {
+	s := &Session{}
+	err := json.Unmarshal(b, s)
+	if err != nil {
+		logrus.Errorln(err)
+		logrus.Errorln("could not unmarshal session from bytes")
+		logrus.Errorln(string(b))
+	}
+	return s
 }
