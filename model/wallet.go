3622a734594ac739b8a85c79050ae859888c4fe9
Just1689 <Just1689@gmail.com>
2020-07-12 21:05:00 +0200

    salt per wallet

--- model/wallet.go
+++ model/wallet.go
@@ -3,4 +3,5 @@ package model
 type Wallet struct {
 	PrivateKeyEncrypted string `json:"privateKeyEncrypted"`
 	PublicKeyPlain      string `json:"publicKeyPlain"`
+	Salt                string `json:"salt"`
 }
