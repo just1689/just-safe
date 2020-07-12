1c7e3b7ce37d07e191df2fdf7348253e3a3d95bb
Just1689 <Just1689@gmail.com>
2020-07-12 21:35:58 +0200

    dont create a wallet when one exists

--- api/transparent-create-wallet-v1.go
+++ api/transparent-create-wallet-v1.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"github.com/just1689/just-safe/controller"
+	"github.com/just1689/just-safe/model"
 	"github.com/sirupsen/logrus"
 	"net/http"
 )
@@ -27,6 +28,14 @@ func createWalletV1(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
+	_, err = model.StorageDriver.ReadFile("wallet.json")
+	if err == nil {
+		logrus.Errorln("wallet already exists")
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	controller.CreateWalletV1("wallet", password)
 
 }
