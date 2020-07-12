801ea0c954b4fcbd32381e246e0605870ddeabe5
Just1689 <Just1689@gmail.com>
2020-07-12 20:53:06 +0200

    random strings

---
+++ util/rand.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=`',.<>/?;:")
+
+func RandStringRunes(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
