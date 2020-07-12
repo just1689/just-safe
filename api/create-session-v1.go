2911e6895cb039a73a85eed2796ef79e447f136d
Just1689 <Just1689@gmail.com>
2020-07-12 19:51:56 +0200

    session api

---
+++ api/create-session-v1.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"encoding/json"
+	"github.com/just1689/just-safe/controller"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func createSessionV1(writer http.ResponseWriter, request *http.Request) {
+	session, err := controller.CreateSession()
+	if err != nil {
+		logrus.Errorln(err)
+		logrus.Errorln("could not create session")
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	session.PrivateKey = ""
+	b, err := json.Marshal(session)
+	if err != nil {
+		logrus.Errorln("could not marshal session")
+		logrus.Errorln(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	writer.Write(b)
+}
--- api/http.go
+++ api/http.go
@@ -8,9 +8,16 @@ import (
 
 func Listen() {
 
+	//Session
+	http.HandleFunc("/api/createSession/v1", createSessionV1)
+
+	//API
 	http.HandleFunc("/api/createWallet/v1", createWalletV1)
 	http.HandleFunc("/api/getPassword/v1", getPasswordV1)
 	http.HandleFunc("/api/addPassword/v1", addPasswordV1)
+
+	//Second-layer encrypted
+	http.HandleFunc("/api/encrypted/addPassword/v1", encryptedAddPasswordV1)
 	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
 
 }
