41e1c47d89702ea415bf520eceb80c19a1d6c1bf
Just1689 <Just1689@gmail.com>
2020-07-12 15:34:58 +0200

    generic driver

--- client/stowc/generic.go
+++ client/stowc/generic.go
@@ -3,40 +3,41 @@ package stowc
 import (
 	"github.com/graymeta/stow"
 	stowgs "github.com/graymeta/stow/google"
+	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
-type GoogleDriver struct {
+type GenericDriver struct {
 	client *stow.Container
 }
 
-func (g *GoogleDriver) Init() {
+func (g *GenericDriver) InitGoogle() {
 	stowLoc, err := stow.Dial(stowgs.Kind, stow.ConfigMap{
 		stowgs.ConfigJSON:      os.Getenv("GOOGLE_JSON"),
 		stowgs.ConfigProjectId: os.Getenv("GOOGLE_PROJECT_ID"),
 	})
 	if err != nil {
-		log.Fatal(err)
+		logrus.Errorln(err)
+		logrus.Errorln("could not connect to cloud storage")
+		panic(err)
 	}
-
 	stowBucket, err := stowLoc.Container(os.Getenv("BUCKET"))
 	if err != nil {
-		log.Fatal(err)
+		logrus.Errorln(err)
+		logrus.Errorln("could not connect to cloud storage")
+		panic(err)
 	}
-
 	g.client = &stowBucket
 
 }
 
-func (g GoogleDriver) ReadFile(path string) (b []byte, err error) {
+func (g GenericDriver) ReadFile(path string) (b []byte, err error) {
 	err = stow.Walk(*g.client, stow.NoPrefix, 100, func(item stow.Item, err error) error {
 		if err != nil {
 			return err
 		}
-		log.Println(item.Name())
-
 		if item.Name() == path {
 			r, err := item.Open()
 			if err != nil {
@@ -47,18 +48,15 @@ func (g GoogleDriver) ReadFile(path string) (b []byte, err error) {
 		}
 		return nil
 	})
-	if err != nil {
-		return
-	}
 	return
 }
 
-func (g GoogleDriver) WriteFile(path string, data []byte) (err error) {
+func (g GenericDriver) WriteFile(path string, data []byte) (err error) {
 	//Implement
 	return nil
 }
 
-func (g GoogleDriver) ListFiles() (out chan string, err error) {
+func (g GenericDriver) ListFiles() (out chan string, err error) {
 	out = make(chan string)
 	err = stow.Walk(*g.client, stow.NoPrefix, 100, func(item stow.Item, err error) error {
 		if err != nil {
