4eeb324b44960acc3b361f535fe524e07e2c5d31
Just1689 <Just1689@gmail.com>
2020-07-16 19:45:18 +0200

    map to item

--- model/Item.go
+++ model/Item.go
@@ -1,8 +1,24 @@
 package model
 
+import (
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+)
+
 type Item struct {
-	Site           string `json:"site"`
-	Username       string `json:"username"`
-	Password       string `json:"password"`
-	WalletPassword string `json:"walletPassword"`
+	Site           string `json:"site,omitempty"`
+	Username       string `json:"username,omitempty"`
+	Password       string `json:"password,omitempty"`
+	WalletPassword string `json:"walletPassword,omitempty"`
+}
+
+func MapToItem(m map[string]string) (item Item, err error) {
+	b, err := json.Marshal(m)
+	if err != nil {
+		logrus.Errorln(err)
+		return
+	}
+	item = Item{}
+	err = json.Unmarshal(b, &item)
+	return
 }
